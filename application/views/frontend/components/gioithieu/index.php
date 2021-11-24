9ea275ba0b52282e012fedff3b1b355325e0e01f
hoanghaimt <nobitarubi@gmail.com>
2021-11-24 23:37:00 +0700

    update code

---
+++ application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,24 @@
+
+<div class="container">
+	<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 list-menu pull-left">
+                    <?php $this->load->view('frontend/modules/category'); ?> 
+                </div>
+            </div>
+            <div class = "col-xs-12 col-sm-12 col-md-9 col-lg-9 product-content" id="list-content">
+				<div class="product-wrap" style="">
+                    <div class="wrap-2" style="margin-bottom: 20px;">
+		
+			<h3>Giới thiệu về <span style="color: #78cccd ">HNSHOP - Shop Thời Trang Quần Áo Đẹp Tp.HCM</span></h3>
+			<p>Shop  thờ trang HNSHOP được thành lập từ tháng 11 năm 2021, là một trong những thương hiệu thời trang nam/ nữ uy tín hàng đầu Việt Nam. Với sự quản lý chặt chẽ, chuyên nghiệp của đội ngủ quản lý; Nỗ lực sáng tạo không ngừng của bộ phận thiết kế, Sự tận tâm của nhân viên bán hàng… là những yếu tố làm nên thương hiệu thời trang 4MEN lớn mạnh như hiện nay..</p>
+			<p>  HN luôn quan niệm thời trang là sự tìm tòi và sáng tạo nên những sản phẩm của HN đều được thiết kế riêng với sự trẻ trung, hiện đại để mang lại guu thời trang hợp mốt nhất cho các bạn trẻ. Các dòng sản phẩm của HN  không ngừng đa dạng về kiểu dáng, mẫu mã từ áo sơ mi, áo thun, áo khoác, áo vest, quần jean, quần tây, quần kaki… đến các phụ kiện thời trang vô cùng sành điệu, tất cả tạo nên vẻ đẹp hoàn hảo, hiện đại, phong cách cho giớ trẻ..</p>
+			<p>Bên cạnh đó HN luôn đặt mình vào tâm lý và quyền lợi của khách hàng để tung ra những sản phẩm thời trang chất lượng tốt nhất, mẫu mã cực đẹp, mới lạ nhưng với giá cả cực kì hấp dẫn, cạnh tranh nhất.</p>
+            <P>Hiện nay, thương hiệu thời trang nam/nữ  HN  phát triển ngày càng lớn mạnh thành một hệ thống với nhiều chi nhánh cửa hàng bán lẻ tại TP HCM. Ngoài ra, nhằm tạo sự tiện lợi mua sắm tối đa cho khách hàng, HN phát triển hệ thống bán hàng online, giao hàng đến tận tay người tiêu dung trên toàn quốc. Giờ đây ngay tại nhà bạn cũng có thể chọn cho mình những sản phẩm phù hợp với sở thích và phong cách qua website: www.hnshop.com.</P>
+		    <p>Đến với HN, chúng tôi luôn tận tâm tư vấn giúp quý khách tìm được những sản phẩm yêu thích, phù hợp với nhu cầu và góp phần tạo nên phong cách cho riêng mình!</p>
+</div>
+<div>
+   <iframe width="1000" height="315" src="https://www.youtube.com/embed/B7zi3nxrWrc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+</div>
+</div>
+</div>
---
+++ application/views/frontend/modules/footer.php
@@ -0,0 +1,76 @@
+<footer id="footer">
+    <div class="news-social">
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <ul class="list-unstyled social pull-right">
+                  <li><a href="https://www.facebook.com/hoanghaimt/"><i class="fab fa-facebook-f" ></i></a></li>
+                  <li><a href="https://twitter.com/hoanghaidev"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                  <li><a href="https://www.youtube.com/channel/UC4gL5h02hf7wXFO5f-S_LKw"><i class="fab fa-youtube"></i></a></li>
+              </ul>
+              </div>
+        </div>
+    </div>
+    <div class="top-footer">
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-6">
+                <div class="col-xs-12 col-sm-6">
+                    <a href="/">
+                        <img src="public/images/logoHN.png" class="img-fix f-logo" alt="green-construction">
+                    </a>
+                   <div class="about-store">
+                    Cửa hàng bán hàng chuyên cung cấp các sản phẩm chất lượng uy tín chất lượng.
+                   </div>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <div class="f-contact">
+                        <h3>Thông tin liên hệ</h3>
+                        <ul class="list-unstyled">
+                            <li class="clearfix">
+                                <i class="fa fa-map-marker"></i>
+                                <span>Xô Viết Nghệ Tĩnh,Phường 25, Quận Bình Thạnh, Tp. HCM</span>
+                            </li>
+                            <li class="clearfix">
+                                <i class="fa fa-phone"></i>
+                                <span>0971293148 - 0335714976</span>
+                            </li>
+                            <li class="clearfix">
+                                <i class="fa fa-envelope"></i>
+                                <span><a href="mailto:sale.24hstore@gmail.com">hnshop@gmail.com</a></span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6">
+                <div class="col-xs-12 col-sm-6">
+                    <h3 class="footer-title">Chính sách</h3>
+                    <ul class="list-unstyled linklists">
+                        <li><a href="chinh-sach">Chính sách thanh toán</a></li>          
+                        <li><a href="chinh-sach">Chính sách vận chuyển</a></li>
+                        <li><a href="chinh-sach">Chính sách đổi trả</a></li>           
+                        <li><a href="chinh-sach">Chính sách bảo hành</a></li>           
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <h3 class="footer-title">Điều khoản</h3>
+                    <ul class="list-unstyled linklists">
+                        <li><a href="dieu-khoan">Điều khoản sử dụng</a></li>           
+                        <li><a href="dieu-khoan">Điều khoản giao dịch</a></li>           
+                        <li><a href="dieu-khoan">Dịch vụ tiện ích</a></li>           
+                        <li><a href="dieu-khoan">Quyền sở hữu trí tuệ</a></li>           
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="payments">
+        <div class="container">
+            <img src="public/images/payment.png" alt="Phương thức thanh toán" class="img-fix">
+        </div>
+    </div>
+    <div class="copyright">
+        © Bản quyền thuộc về H&N Media | Phát triển phần mềm mã nguồn mở
+       
+    </div>
+</footer>
